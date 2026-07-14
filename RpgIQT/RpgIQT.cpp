00c30cde1152f463f4d733694b800d1bcdd01c67
Merdan <wisew.tube@gmail.com>
2026-07-14 22:43:30 +0500

    Update RpgIQT.cpp

--- RpgIQT/RpgIQT.cpp
+++ RpgIQT/RpgIQT.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>;
 #include <vector>;
+#include <string>;
+#include <fstream>;
+#include <sstream>;
 
 using namespace std;
 
@@ -9,6 +12,14 @@ class Item {
     string type;
 };
 
+
+class Quest{
+    string title;
+    string description;
+    int goldReward;
+    bool completed;
+};
+
 class Character {
     string name;
     int hp, level, gold;
@@ -16,12 +27,27 @@ class Character {
     vector<string> completedQuests;
 };
 
-class Quest {
-    string title;
-    string description;
-    int goldReward;
-    bool completed;
-};
+void saveGame(const vector<Character> roster) {
+    ofstream outFile("save.amq");
+
+    for (const Character& character : roster) {
+        outFile << "CHARACTER," << character.name << "," << character.hp << "," << character.level << "," << character.gold << endl;
+
+        for (const Item& item : character.inventory) {
+            outFile << "ITEM," << item.name << "," << item.value << "," << item.type << endl;
+        }
+
+        for (const Quest& quest : character.quests) {
+            outFile << "QUEST," << quest.title << "," << quest.reward << "," << quest.completed << endl;
+        }
+    }
+
+    outFile.close();
+}
+
+void loadGame() {
+
+}
 
 int main() {
 
