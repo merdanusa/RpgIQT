56b70c7e7ad65ee88e63a7fdf717da22ad682d6d
Merdan <wisew.tube@gmail.com>
2026-07-14 22:32:55 +0500

    Update RpgIQT.cpp

--- RpgIQT/RpgIQT.cpp
+++ RpgIQT/RpgIQT.cpp
@@ -1,4 +1,27 @@
 ﻿#include <iostream>;
+#include <vector>;
+
+using namespace std;
+
+class Item {
+    string name;
+    int value;
+    string type;
+};
+
+class Character {
+    string name;
+    int hp, level, gold;
+    vector<Item> inventory;
+    vector<string> completedQuests;
+};
+
+class Quest {
+    string title;
+    string description;
+    int goldReward;
+    bool completed;
+};
 
 int main() {
 
